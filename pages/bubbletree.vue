f617f0b2b5ec199c4ca5c1be75a344ce6e03a566
ak2ie <mail@ak2ie.net>
2022-01-14 21:06:48 +0900

    GSAPインストール

---
+++ pages/bubbletree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="test-title">test</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { gsap } from 'gsap'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: '使途予算別',
+    }
+  },
+  mounted() {
+    gsap.to('#test-title', { x: 500, duration: 3 })
+  },
+})
+</script>
