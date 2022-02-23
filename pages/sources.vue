984c223fb683274156fcf044342bd74ac3900d50
ak2ie <mail@ak2ie.net>
2022-02-23 16:22:01 +0900

    予算名称を反映

--- pages/sources.vue
+++ pages/sources.vue
@@ -8,9 +8,7 @@
     <div class="row justify-content-center mb-3">
       <div class="col-lg-7">
         <p>
-          このサイトは、{{ year }}年度の自治体の{{
-            budgetName
-          }}の事業名単位のデータを用いています。
+          このサイトは、{{ budgetName }}の事業名単位のデータを用いています。
         </p>
       </div>
     </div>
@@ -34,7 +32,7 @@
     <div class="row justify-content-center mb-3">
       <div class="col-lg-7">
         <p>
-          自分の年間総収入を入力し、単身世帯かそれ以外かを選択すると、給与所得者であるという前提で、年間に支払う税年総額と一日当たりに支払っている税額が表示されます。一日当たりの税額は10分野に分類されています。また、１０の各分野毎に支払われている税額は、それぞれ、主要な事業予算を担当している税額に細分類されています。
+          自分の年間総収入を入力し、単身世帯かそれ以外かを選択すると、給与所得者であるという前提で、年間に支払う年間総税額と一日当たりに支払っている税額が表示されます。一日当たりの税額は10分野に分類されています。また、１０の各分野毎に支払われている税額は、それぞれ、主要な事業予算を担当している税額に細分類されています。
           <br />
           所得控除額は、単身世帯の場合33万円、扶養一人世帯の場合66万円、税負担額は課税対象額の6％と設定しています。また、10分野への税金の配分比率は、つくば市の一般会計全体における10分野への予算配分比率({{
             year
@@ -72,6 +70,10 @@ type DataType = {
    * 予算年度
    */
   year: number
+  /**
+   * 予算名称
+   */
+  budgetName: string
 }
 
 export default Vue.extend({
@@ -79,6 +81,7 @@ export default Vue.extend({
     return {
       region: this.$accessor.regionCofogData.regionCofogData.governmentName,
       year: this.$accessor.regionCofogData.regionCofogData.year,
+      budgetName: this.$accessor.regionCofogData.regionCofogData.budgetName,
     }
   },
   head() {
