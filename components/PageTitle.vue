0f452a53a8961486461da002f186fd75ff0f3866
Kazuki Imamura <kaizumaki@gmail.com>
2022-02-23 17:58:08 +0900

    固定ページ用のページタイトルコンポーネントを追加

---
+++ components/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="page-title fs-5">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.page-title {
+  position: relative;
+  margin: 20px 0 60px;
+  padding-bottom: 10px;
+
+  &::after {
+    position: absolute;
+    content: ' ';
+    display: block;
+    border-bottom: 5px solid $primary;
+    bottom: -3px;
+    width: 50px;
+  }
+}
+</style>
