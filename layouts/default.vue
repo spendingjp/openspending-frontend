f6784379c51b6e598ea8c2447bb0a215fc4f52d7
Kazuki Imamura <kaizumaki@gmail.com>
2022-02-26 22:50:03 +0900

    styleでcss variablesにcolorCodeをセット

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,19 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      defaultPrimary: '#BF9D73',
+      defaultSecondary: '#4369D9',
+    }
+  },
+  mounted() {
+    const primary = this.$store.state.regionCofogData.primaryColorCode ?? this.defaultPrimary
+    const secondary = this.$store.state.regionCofogData.secondaryColorCode ?? this.defaultSecondary
+    document.documentElement.setAttribute('style', `--color-primary: ${primary}; --color-secondary: ${secondary};`);
+  }
+})
 </script>
 
 <style>
