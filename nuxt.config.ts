e25b9e19813181c65783c0d0bf6a6b2caa33cb4d
TAKANO Mitsuhiro <takano32@gmail.com>
2022-02-26 19:07:50 +0900

    Use APPLICATION_API_URL environment variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,8 @@ const config: NuxtConfig = {
 
   publicRuntimeConfig: {
     axios: {
-      baseURL: process.env.API_BASE_URL || 'https://openspending.net/api/v1/',
+      baseURL: process.env.APPLICATION_API_URL + 'api/v1/' || 'https://openspending.net/api/v1/',
+
     },
   },
 
